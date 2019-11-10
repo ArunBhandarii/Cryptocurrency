397e40ca2b45a68918911aee2cecd79a74f35ed9
Arun Bhandari <24475929+LightC0der@users.noreply.github.com>
2019-11-10 17:03:37 +1100

    Update strings.py

--- strings.py
+++ strings.py
@@ -15,8 +15,8 @@ active_currencies = results['data']['active_cryptocurrencies']
 active_markets = results['data']['active_markets']
 bitcoin_percentage = results['data']['bitcoin_percentage_of_market_cap']
 last_updated = results['data']['last_updated']
-global_cap = results['data']['quotes']['USD']['total_market_cap']
-global_volume = results['data']['quotes']['USD']['total_volume_24h']
+global_cap = str(results['data']['quotes']['USD']['total_market_cap'])
+global_volume = str(results['data']['quotes']['USD']['total_volume_24h'])
 
 active_currencies_string = '{:,}'.format(active_currencies)
 active_markets_string = '{:,}'.format(active_markets)
