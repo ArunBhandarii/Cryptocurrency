c077f19074a39c5dd0d3df1b5884e3422128abf1
Arun Bhandari <24475929+LightC0der@users.noreply.github.com>
2019-11-10 15:39:36 +1100

    Create api_var.py
    It prints the active cryptocurrencies !

---
+++ api_var.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+global_url = "https://api.coinmarketcap.com/v2/global/"
+
+request = requests.get(global_url)
+results = request.json()
+
+# print(json.dumps(results, sort_keys=True, indent=4))
+
+active_currencies = requests['data']['active_cryptocurrencies']
+
+print(active_currencies)
