f19e4af1285f96ec441f915f0f3dfdd155ca9cb8
Arun Bhandari <24475929+LightC0der@users.noreply.github.com>
2019-11-10 15:31:49 +1100

    Create global_api.py

---
+++ global_api.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+global_url = "https://api.coinmarketcap.com/v2/global/"
+
+request = requests.get(global_url)
+results = request.json()
+
+print{json.dumps{results, sort_keys=True, indent=4}}
