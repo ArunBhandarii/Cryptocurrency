4bfd4df47b7d2e37a2c38b915b9d6681749ca61c
Arun Bhandari <24475929+LightC0der@users.noreply.github.com>
2019-12-18 11:49:22 +1100

    Update global_api.py

--- global_api.py
+++ global_api.py
@@ -6,4 +6,4 @@ global_url = "https://api.coinmarketcap.com/v2/global/"
 request = requests.get(global_url)
 results = request.json()
 
-print(json.dumps(results, sort_keys=True, indent=4))
+print( json.dumps( results, sort_keys=True, indent=4))
