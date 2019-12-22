5e5e2d11ed3820c951291704792ba1f47c02c786
Arun Bhandari <24475929+LightC0der@users.noreply.github.com>
2019-12-22 13:55:02 +1100

    Update strings.py

--- strings.py
+++ strings.py
@@ -2,7 +2,7 @@ import json
 import requests
 from datetime import datetime
 
-global_url = "https://api.coinmarketcap.com/v2/global/"
+global_url = " https://api.coinmarketcap.com/v2/global/"
 
 request = requests.get(global_url)
 results = request.json()
