74319ea4bda28a82c26c6ea63500e3768681257c
Arun Bhandari <24475929+LightC0der@users.noreply.github.com>
2020-01-03 12:57:02 +1100

    Update strings.py

--- strings.py
+++ strings.py
@@ -21,7 +21,7 @@ global_volume = str(results['data']['quotes']['USD']['total_volume_24h'])
 
 active_currencies_string = '{:,}'.format(active_currencies)
 active_markets_string = '{:,}'.format(active_markets)
-global_cap_string = '{:,}'.format(global_cap)
+global_cap_string = '{:,}'.format (global_cap)
 global_volume_string = '{:,}'.format(global_volume)
 
 last_updated_string = datetime.frontimestamp(last_updated).strftime('%B %d, %Y at %I:%M%p')
