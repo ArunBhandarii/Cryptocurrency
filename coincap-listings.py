9ea2f87974024d85285f1cd7912b9f966778aad3
Arun Bhandari <24475929+LightC0der@users.noreply.github.com>
2020-01-20 03:39:15 +1100

    Update coincap-listings.py

--- coincap-listings.py
+++ coincap-listings.py
@@ -4,7 +4,7 @@ import requests
 listing_url = 'https://api.coinmarketcap.com/v2/listings/'
 
 request = requests.get(listing_url)
-results = request.json()
+results = request.json ()
 
 # print(json.dumps(results, sort_keys=True, indent=4))
 
